7c3864d8443ea445e4d2dbbe8b403e928a96654b
Mac <yaroslav1992r@gmail.com>
2025-05-21 22:13:22 +0300

    Add 'articles' app

--- hexlet_django_blog/settings.py
+++ hexlet_django_blog/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hexlet_django_blog'
+    'hexlet_django_blog',
+    'hexlet_django_blog.article',
+    
 ]
 
 MIDDLEWARE = [
